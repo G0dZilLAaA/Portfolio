5780c44ecd737d94bfbfaba3e89051b07ad18950
G0dZilLAaA <kumawatmohit435@gmail.com>
2026-05-25 09:53:00 +0530

    the start of the project !!

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My First Full Stack Project</h1>
+      <p>React Frontend + Node Backend deployed on Render</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
